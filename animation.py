3033aedae134dafd0062efc1d60145b20486d8a4
vkamei99 <vikmotakamei@gmail.com>
2024-12-28 16:53:38 -0300

    teste de animação usando FuncAnimation()

--- animation.py
+++ animation.py
@@ -2,3 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+t = np.linspace(0, 10, 100)
+y = np.sin(t)
+
+fig, ax = plt.subplots()
+
+ax.set_xlim([min(t) , max(t)])
+ax.set_ylim(-2, 2)
+
+animated_plot, = ax.plot([],[])
+
+def update_data(frame):
+    animated_plot.set_data(t[:frame], y[:frame])
+
+    return animated_plot,
+
+animation = FuncAnimation(
+    fig= fig,
+    func= update_data,
+    frames=len(t),
+    interval=25
+)
+
+plt.show()
